eab3603ae947cab085519498d7ec726358fa4d7e
yoga setiawan <yogainformatika@gmail.com>
2019-10-18 13:09:41 +0700

    disable Notif when open chat room

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -1,6 +1,6 @@
 import xs from "xstream";
 import firebase from "react-native-firebase";
-import { Platform } from "react-native";
+import {Platform, AsyncStorage} from "react-native";
 
 import * as Qiscus from "qiscus";
 
@@ -8,112 +8,115 @@ const getToken = () => firebase.messaging().getToken();
 export const getToken$ = () => xs.from(getToken());
 
 export const initiate$ = () =>
-  Qiscus.isLogin$()
-    .map(() => getToken$())
-    .flatten()
-    .take(1)
-    .map(token => xs.from(Qiscus.setDeviceToken(token)))
-    .flatten();
+    Qiscus.isLogin$()
+        .map(() => getToken$())
+        .flatten()
+        .take(1)
+        .map(token => xs.from(Qiscus.setDeviceToken(token)))
+        .flatten();
 
 async function requestPermission() {
-  const enabled = await firebase.messaging().hasPermission();
-  if (!enabled) {
-    await firebase.messaging().requestPermission();
-  }
+    const enabled = await firebase.messaging().hasPermission();
+    if (!enabled) {
+        await firebase.messaging().requestPermission();
+    }
 }
 
 export function createChannel() {
-  if (Platform.OS === "android") {
-    const channel = new firebase.notifications.Android.Channel(
-      "test-channel",
-      "Test Channel",
-      firebase.notifications.Android.Importance.Max
-    ).setDescription("My apps test channel");
-    firebase.notifications().android.createChannel(channel);
-  }
+    if (Platform.OS === "android") {
+        const channel = new firebase.notifications.Android.Channel(
+            "test-channel",
+            "Test Channel",
+            firebase.notifications.Android.Importance.Max
+        ).setDescription("My apps test channel");
+        firebase.notifications().android.createChannel(channel);
+    }
 }
 
 export const onNotification$ = () =>
-  xs.create({
-    listener: null,
-    start(listener) {
-      this.listener = firebase.notifications().onNotification(notification => {
-        listener.next(notification);
-      });
-    },
-    stop() {
-      if (this.listener != null) this.listener();
-    }
-  });
+    xs.create({
+        listener: null,
+        start(listener) {
+            this.listener = firebase.notifications().onNotification(notification => {
+                AsyncStorage.getItem('currentRoomId',(error,result)=>{
+                        if (notification.data.qiscus_room_id != result ){
+                            listener.next(notification);
+                        }
+                });
+            });
+        },
+        stop() {
+            if (this.listener != null) this.listener();
+        }
+    });
 export const onNotificationDisplayed$ = () =>
-  xs.create({
-    listener: null,
-    start(listener) {
-      this.listener = firebase
-        .notifications()
-        .onNotificationDisplayed(notification => {
-          listener.next(notification);
-        });
-    },
-    stop() {
-      if (this.listener != null) this.listener();
-    }
-  });
-export const onNotificationOpened$ = () =>
-  xs.create({
-    listener: null,
-    start(listener) {
-      this.listener = firebase
-        .notifications()
-        .onNotificationOpened(notification => {
-          listener.next(notification);
-        });
-    },
-    stop() {
-      if (this.listener != null) this.listener();
-    }
-  });
-export const createNotification = notif => {
-  const notification = new firebase.notifications.Notification()
-    .setNotificationId(notif.notificationId)
-    .setTitle(notif.title)
-    .setBody(notif.body)
-    .setData(notif.data);
-  if (Platform.OS === "android") {
-    notification.android
-      .setChannelId("test-channel")
-      .android.setSmallIcon("ic_launcher");
-  }
+    xs.create({
+        listener: null,
+        start(listener) {
 
-  return notification;
+            this.listener = firebase.notifications().onNotificationDisplayed(notification => {
+                listener.next(notification);
+            });
+
+        },
+        stop() {
+            if (this.listener != null) this.listener();
+        }
+    });
+export const onNotificationOpened$ = () =>
+    xs.create({
+        listener: null,
+        start(listener) {
+            this.listener = firebase
+                .notifications()
+                .onNotificationOpened(notification => {
+                    listener.next(notification);
+                });
+        },
+        stop() {
+            if (this.listener != null) this.listener();
+        }
+    });
+export const createNotification = notif => {
+    const notification = new firebase.notifications.Notification()
+        .setNotificationId(notif.notificationId)
+        .setTitle(notif.title)
+        .setBody(notif.body)
+        .setData(notif.data);
+    if (Platform.OS === "android") {
+        notification.android
+            .setChannelId("test-channel")
+            .android.setSmallIcon("ic_launcher");
+    }
+
+    return notification;
 };
 export const requestPermission$ = () => xs.from(requestPermission());
 
-export const displayNotification = notification =>
-  firebase.notifications().displayNotification(notification);
-export const getInitialNotification = () =>
-  firebase.notifications().getInitialNotification();
+export const displayNotification = notification => firebase.notifications().displayNotification(notification);
+export const getInitialNotification = () => firebase.notifications().getInitialNotification();
 
 export function start(onDestroy) {
-  requestPermission();
-  createChannel();
+    requestPermission();
+    createChannel();
 
-  const listener2 = firebase.notifications().onNotification(notif => {
-    const notification = new firebase.notifications.Notification()
-      .setNotificationId(notif.notificationId)
-      .setTitle(notif.title)
-      .setBody(notif.body)
-      .setData(notif.data);
+    const listener2 = firebase.notifications().onNotification(notif => {
+        const notification = new firebase.notifications.Notification()
+            .setNotificationId(notif.notificationId)
+            .setTitle(notif.title)
+            .setBody(notif.body)
+            .setData(notif.data);
 
-    if (Platform.OS === "android") {
-      notification.android
-        .setChannelId("test-channel")
-        .android.setSmallIcon("ic_launcher");
-    }
+        if (Platform.OS === "android") {
+            notification.android
+                .setChannelId("test-channel")
+                .android.setSmallIcon("ic_launcher");
+        }
 
-    firebase.notifications().displayNotification(notification);
-  });
-  const listener3 = firebase
-    .notifications()
-    .onNotificationOpened(notification => {});
+        firebase.notifications().displayNotification(notification);
+    });
+    const listener3 = firebase
+        .notifications()
+        .onNotificationOpened(notification => {
+        });
 }
